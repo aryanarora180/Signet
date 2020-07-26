36ac2d37a501f2370a6cd84228017d13c7aa6a28
sakets3010 <sakets3010@gmail.com>
2020-07-26 15:43:49 +0530

    added viewmodels for login fragments.

---
+++ app/src/main/java/com/example/signet/login/SignInViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.signet.login
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class SignInViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val _firebaseAuth = Firebase.auth
+
+    private val googleSignInSuccess: MutableLiveData<Boolean> = MutableLiveData()
+    fun signInWithGoogle(task: Task<GoogleSignInAccount>): LiveData<Boolean> {
+        googleSignInSuccess.value = null
+        try {
+            val account = task.getResult(ApiException::class.java)
+            if (account != null) {
+                firebaseAuthWithGoogle(account)
+            }
+        } catch (e: ApiException) {
+            googleSignInSuccess.value = false
+        } finally {
+            return googleSignInSuccess
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        _firebaseAuth.signInWithCredential(credential).addOnCompleteListener {
+            googleSignInSuccess.value = it.isSuccessful
+        }
+    }
+
+}
