f6a72f0149502557a29261946c94a1975a9233c1
sakets3010 <sakets3010@gmail.com>
2020-07-26 14:21:25 +0530

    added splash activity.

--- app/src/main/java/com/example/signet/SplashActivity.kt
+++ app/src/main/java/com/example/signet/SplashActivity.kt
@@ -1,11 +1,30 @@
 package com.example.signet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val signedInAccount = auth.currentUser
+        if(signedInAccount!=null) {
+            //User has signed in before. Redirecting to MainActivity
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+        else {
+            //User hasn't signed in before. Redirecting to LoginActivity
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
